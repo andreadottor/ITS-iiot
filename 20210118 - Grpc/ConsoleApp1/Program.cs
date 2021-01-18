468a4b030a60b6e90dbdaffdfa016aaabcf79242
Andrea Dottor <andrea@dottor.net>
2021-01-18 12:36:07 +0100

    grpc

---
+++ 20210118 - Grpc/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    using Grpc.Net.Client;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var result = client.SayHello(new HelloRequest() { 
+                Name = "Andrea"
+            });
+
+            Console.WriteLine(result.Message);
+            Console.ReadLine();
+
+        }
+    }
+}
